dd49ca85de72fb08510b73decbe86e39d0c3ed8b
HaoRan-doc <hhr87965100@163.com>
2023-03-13 11:36:48 +0800

    Update App.js
    666

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { Collapse } from 'antd';
 const { Header, Content, Sider } = Layout;
 const items1 = ['1', '2', '3'].map((key) => ({
   key,
-  label: `nav ${key}`,
+  label: `na ${key}`,
 }));
 const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
   const key = String(index + 1);
