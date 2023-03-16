5081f796b67e440a7416e91526ec7a84212f2ae2
HaoRan-doc <hhr87965100@163.com>
2023-03-16 13:30:27 +0800

    fix-更新

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import './App.css';
-import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import {
+  LaptopOutlined,
+  NotificationOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, theme, Carousel, Collapse } from 'antd';
 import React from 'react';
-import { Carousel } from 'antd';
-import { Collapse } from 'antd';
-
+import MyFirstComponent from './components/my-first-component';
+import MyTwoComponent from './components/my-two-component';
 
 const { Header, Content, Sider } = Layout;
 const items1 = ['1', '2', '3'].map((key) => ({
   key,
-
-  label: `naV ${key}`,
-
-  label: `na ${key}`,
-
+  label: `nav ${key}`,
 }));
-const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
-  const key = String(index + 1);
-  return {
-    key: `su${key}`,
-    icon: React.createElement(icon),
-    label: `subnav ${key}`,
-    children: new Array(4).fill(null).map((_, j) => {
-      const subKey = index * 4 + j + 1;
-      return {
-        key: subKey,
-        label: `option${subKey}`,
-      };
-    }),
-  };
-});
+const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map(
+  (icon, index) => {
+    const key = String(index + 1);
+    return {
+      key: `sub${key}`,
+      icon: React.createElement(icon),
+      label: `subnav ${key}`,
+      children: new Array(4).fill(null).map((_, j) => {
+        const subKey = index * 4 + j + 1;
+        return {
+          key: subKey,
+          label: `option${subKey}`,
+        };
+      }),
+    };
+  }
+);
 const contentStyle = {
   margin: 0,
   height: '160px',
@@ -39,15 +39,12 @@ const contentStyle = {
   background: '#364d79',
 };
 const { Panel } = Collapse;
+
 const text = `
   A dog is a type of domesticated animal.
   Known for its loyalty and faithfulness,
   it can be found as a welcome guest in many households across the world.
 `;
-
-
-
-
 
 const App = () => {
   const {
@@ -56,12 +53,19 @@ const App = () => {
   const onChange = (currentSlide) => {
     console.log(currentSlide);
   };
-
+  const onCollapseChange = (key) => {
+    console.log(key);
+  };
   return (
     <Layout>
       <Header className="header">
         <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1} />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={items1}
+        />
       </Header>
       <Layout>
         <Sider
@@ -103,41 +107,40 @@ const App = () => {
               background: colorBgContainer,
             }}
           >
-    <Carousel afterChange={onChange}>
-      <div>
-        <h3 style={contentStyle}>1</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>2</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>3</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>4</h3>
-      </div>
-    </Carousel>
-    <Collapse defaultActiveKey={['1']} onChange={onChange}>
-      <Panel header="This is panel header 1" key="1">
-        <p>{text}</p>
-      </Panel>
-      <Panel header="This is panel header 2" key="2">
-        <p>{text}</p>
-      </Panel>
-      <Panel header="This is panel header 3" key="3">
-        <p>{text}</p>
-      </Panel>
-    </Collapse>             
+            {/* <Carousel afterChange={onChange}>
+              <div>
+                <h3 style={contentStyle}>1</h3>
+              </div>
+              <div>
+                <h3 style={contentStyle}>2</h3>
+              </div>
+              <div>
+                <h3 style={contentStyle}>3</h3>
+              </div>
+              <div>
+                <h3 style={contentStyle}>4</h3>
+              </div>
+            </Carousel>
+            <div style={{ height: 100 }}></div>
+            <Collapse defaultActiveKey={['1']} onChange={onChange}>
+              <Panel header="This is panel header 1" key="1">
+                <p>{text}</p>
+              </Panel>
+              <Panel header="This is panel header 2" key="2">
+                <p>{text}</p>
+              </Panel>
+              <Panel header="This is panel header 3" key="3">
+                <p>{text}</p>
+              </Panel>
+            </Collapse> */}
+            <MyFirstComponent propsName={'propsName'} />
+            <MyTwoComponent propsName={'propsName 1'} />
+            <MyTwoComponent propsName={'propsName 2'} />
+            <MyTwoComponent propsName={'propsName 3'} />
           </Content>
-          
         </Layout>
       </Layout>
     </Layout>
   );
-
-  
-  
 };
-
-
-export default App; 
+export default App;
